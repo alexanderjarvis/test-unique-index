a4547aa2158747fc815c072108217261bf542e5d
Alex Jarvis <alex@panaxiom.co.uk>
2012-10-28 13:52:22 +0000

    Added 'findAndCreate' to test manually testing for presence of collection, rather than relying on the mongodb unique index

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -5,10 +5,10 @@ import play.api.mvc._
 import play.api.Play.current
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.ReactiveMongoPlugin
-
 import reactivemongo.api._
 import reactivemongo.bson._
 import reactivemongo.bson.handlers.DefaultBSONHandlers._
+import scala.concurrent.Future
 
 object Users extends Controller with MongoController {
   
@@ -23,6 +23,17 @@ object Users extends Controller with MongoController {
         else
           BadRequest
       }.recover{ case _ => InternalServerError}
+    }
+  }
+  
+  def findAndCreate = Action {
+    Async {
+      collection.find(BSONDocument("email" -> BSONString("test@example.com"))).headOption.flatMap { user =>
+        user match {
+          case Some(user) => Future(BadRequest)
+          case None => collection.insert(BSONDocument("email" -> BSONString("test@example.com"))).map(_ => Ok)
+        }
+      }
     }
   }
   
